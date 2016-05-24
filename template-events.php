e62976610801dd0aedc8a714129685fcf8fcb4cf
samuraijaffy <jaffyescarcha@Jaffys-MacBook-Pro.local>
2016-05-24 11:35:45 -0400

    Add Updated Events Calendar to Events Template

--- template-events.php
+++ template-events.php
@@ -304,7 +304,7 @@ Template Name: Events
 	<script>
 
 		(function() {
-			var flickerAPI = "https://events.ucf.edu/calendar/1991/diversity-and-inclusion/upcoming/feed.json";
+			var flickerAPI = "https://events.ucf.edu/calendar/2829/office-of-diversity-and-inclusion/this-month/feed.json";
 			$.getJSON( flickerAPI, {
 			    format: "json"
 			})
