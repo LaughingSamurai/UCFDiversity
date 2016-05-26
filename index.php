cdaf57e08e9181b6fc00c90e26adab07b7a1f6a7
samuraijaffy <jaffyescarcha@Jaffys-MacBook-Pro.local>
2016-05-26 10:52:07 -0400

    Setting up structure for blogroll and single post page

--- index.php
+++ index.php
@@ -1,4 +1,44 @@
-<?php get_template_part('templates/page', 'header'); ?>
-<?php if (have_posts()) : ?>
-  <?php get_template_part('templates/content', get_post_format()); ?>
-<?php endif; ?>
+<div id="news-content-container">
+
+	<div id="page-content" class="container">
+
+		<div id="news-roll" class="col-ld-9 col-md-9 col-sm-9 col-xs-12">
+
+			<h1>News</h1>
+
+			<hr>
+
+			<?php wp_reset_query(); ?>
+
+			<?php if (have_posts()) : ?>
+
+				<?php while (have_posts()) : the_post(); ?>
+
+					<span><?php the_category( ); ?></span>
+
+					<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+					<p><?php the_excerpt(); ?></p>
+
+				<?php endwhile; ?>
+					
+			<?php endif; ?>
+
+			<div class="clear"></div>
+
+		</div>
+
+		<div id="news-sidebar" class="col-ld-3 col-md-3 col-sm-3 col-xs-12">
+
+			<ul>
+			    <?php wp_list_categories( array(
+			        'orderby' => 'name',
+			        'exclude' => array( 1 )
+			    ) ); ?> 
+			</ul>
+
+		</div>
+
+	</div>
+
+</div>
--- templates/content-single.php
+++ templates/content-single.php
@@ -1,16 +1,87 @@
-<?php while (have_posts()) : the_post(); ?>
-  <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
-      <?php the_content(); ?>
+<div id="news-content-container">
+
+  <div id="page-content" class="container">
+
+    <?php $actual_link = 'http' . (isset($_SERVER['HTTPS']) ? 's' : '') . '://' . "{$_SERVER['HTTP_HOST']}/{$_SERVER['REQUEST_URI']}" ?>
+
+    <ul id="social-container">
+      <li class="rrssb-facebook">
+              <!-- Replace with your URL. For best results, make sure you page has the proper FB Open Graph tags in header:
+                          https://developers.facebook.com/docs/opengraph/howtos/maximizing-distribution-media-content/ -->
+              <a href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $actual_link; ?>" class="popup facebook-share-icon">
+                  <span class="rrssb-icon">
+                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/facebook.png" alt="Facebook">
+                  </span>
+                  <span class="rrssb-text">Share</span>
+              </a>
+          </li>
+          <li class="rrssb-twitter">
+              <!-- Replace href with your Meta and URL information  -->
+              <a href="http://twitter.com/home?status=UCF%20Office%20of%20Diversity%20%26%20Inclusion%20<?php echo $actual_link; ?>" class="popup twitter-share-icon">
+                  <span class="rrssb-icon">
+                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter.png" alt="Twitter">
+                  </span>
+                  <span class="rrssb-text">Tweet</span>
+              </a>
+          </li>
+          <li class="rrssb-googleplus">
+              <!-- Replace href with your meta and URL information.  -->
+              <a href="https://plus.google.com/share?url=Check%20out%20how%20ridiculously%20responsive%20these%20social%20buttons%20are%20<?php echo $actual_link; ?>" class="popup google-share-icon">
+                  <span class="rrssb-icon">
+                      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/google-plus.png" alt="Google +">
+                  </span>
+                  <span class="rrssb-text">Share</span>
+              </a>
+          </li>
+    </ul>
+
+    <div class="clear"></div>
+
+    <div id="news-roll" class="col-ld-9 col-md-9 col-sm-9 col-xs-12">
+
+      <?php while (have_posts()) : the_post(); ?>
+        <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
+          <header>
+
+          <div class="blog-single-title-container">
+
+            <div class="blog-single-title-content">
+
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+
+            </div>
+
+          </div>
+
+          </header>
+          <div class="entry-content row">
+
+            <div class="single-post-content">
+
+              <?php the_post_thumbnail( 'full' ); ?>
+              
+              <?php the_content(); ?>
+
+            </div>
+
+          </div>
+
+        </article>
+      <?php endwhile; ?>
+
     </div>
-    <footer>
-      <?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-      <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
-    </footer>
-    <?php comments_template('/templates/comments.php'); ?>
-  </article>
-<?php endwhile; ?>
+
+    <div id="news-sidebar" class="col-ld-3 col-md-3 col-sm-3 col-xs-12">
+
+      <ul>
+          <?php wp_list_categories( array(
+              'orderby' => 'name',
+              'exclude' => array( 1 )
+          ) ); ?> 
+      </ul>
+
+    </div>
+
+  </div>
+
+</div>
