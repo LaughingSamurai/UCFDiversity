f81558917cf3d43cdf04ec3e08fbee41eefab6d8
samuraijaffy <jaffyescarcha@Jaffys-MacBook-Pro.local>
2016-05-26 15:14:45 -0400

    Further Styling of Newsroll

--- index.php
+++ index.php
@@ -15,14 +15,18 @@
 				<?php while (have_posts()) : the_post(); ?>
 
 					<div class="newsroll-single-container">
+
+						<div class="inner">
 						
-						<span><?php the_category( ); ?></span>
+							<span><?php the_category( ); ?></span>
 
-						<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+							<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
-						<p><?php the_excerpt(); ?></p>
+							<p><?php the_excerpt(); ?></p>
 
-						<a href="<?php the_permalink(); ?>" class="newsroll-red-more">Read more <img src="<?php echo get_template_directory_uri(); ?>/assets/img/black-arrow.png" alt="<?php the_title(); ?>"></a>
+							<a href="<?php the_permalink(); ?>" class="newsroll-red-more">Read more <img src="<?php echo get_template_directory_uri(); ?>/assets/img/black-arrow.png" alt="<?php the_title(); ?>"></a>
+
+						</div>
 
 						<hr>
 
