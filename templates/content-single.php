9c41a005811c28c145a41208071695537d0cd521
samuraijaffy <jaffyescarcha@Jaffys-MacBook-Pro.local>
2016-06-06 15:43:00 -0400

    Show post title by default on single post page

--- templates/content-single.php
+++ templates/content-single.php
@@ -101,7 +101,7 @@
         <?php while (have_posts()) : the_post(); ?>
           <article <?php post_class() ?> id="post-<?php the_ID(); ?>">
             
-            <h1><?php the_field('page_title'); ?></h1>
+            <h1><?php the_title(); ?></h1>
 
             <span class="article-meta"><?php the_date( 'F j, Y' ); ?></span>
 
